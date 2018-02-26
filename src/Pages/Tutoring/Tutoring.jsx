ce434932956c514d6adad53270208e04c85f24b8
Starr Yang <starrtyang@berkeley.edu>
2018-02-25 21:08:43 -0800

    Fixes looking at other parts of the calendar

--- src/Pages/Tutoring/Tutoring.jsx
+++ src/Pages/Tutoring/Tutoring.jsx
@@ -20,6 +20,8 @@ const API_KEY = "AIzaSyCPRfO9EWrZeIYtb2Ql2Ku31JHxBC7zORU";
 let url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}`
 let tutoring = /[\w ][ \t]*-[ \t]*[\w]/
 
+let allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k])
+
 function getEvents(callback){
   request
     .get(url)
@@ -74,7 +76,10 @@ class Tutoring extends React.Component {
                 {height: '60em',
                 width: '100%'}
               }
-              date={new Date()}
+              defaultDate={new Date()}
+              views={allViews}
+              step={60}
+              showMultiDayTimes
               events={this.state.events}/>
             </center>
     </div>
