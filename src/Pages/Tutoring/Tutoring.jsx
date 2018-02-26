77d8234f0e9c450907d5dfdb35b7ec801419684b
Starr Yang <starrtyang@berkeley.edu>
2018-02-25 17:21:46 -0800

    Implemented tutoring page
    NOTE: Isn't linked to in the header and doesn't work right now since I removed the API key
    
    todo? find the way you're suppose to have your api keys? Something about a proxy server?

---
+++ src/Pages/Tutoring/Tutoring.jsx
@@ -0,0 +1,82 @@
+import * as React from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {withRouter} from 'react-router';
+import {Guac} from 'guac-hoc/lib/Guac';
+
+import {Card, CardTextArea} from 'yui-md/lib/Card';
+import {Divider} from 'yui-md/lib/Divider';
+
+import {_PaperBorder} from './_PaperBorder';
+
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+import request from 'superagent';
+
+BigCalendar.momentLocalizer(moment);
+
+const CALENDAR_ID = "berkeley.edu_ovluroab46af2sbup6ot2h6m58@group.calendar.google.com";
+const API_KEY = "INSERT_API_KEY";
+let url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}`
+
+function getEvents(callback){
+  request
+    .get(url)
+    .end((err, resp) => {
+      if (!err) {
+        let events = [];
+        JSON.parse(resp.text).items.map((event) => {
+          if(event.start !== undefined && event.end !== undefined){
+            events.push({
+              start: new Date(event.start.dateTime),
+              end: new Date(event.end.dateTime),
+              title: event.summary || "",
+            })
+        }
+        })
+        console.log(events);
+        callback(events)
+      }
+    })
+}
+
+//require('style-loader!css!react-big-calendar/lib/css/react-big-calendar.css')
+class Tutoring extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      events: []
+    } 
+    this.bindAllMethods();
+  }
+
+  componentDidMount () {
+    getEvents((events) => {
+      this.setState({events});
+    })
+  }
+
+  render() {
+    return (
+      <div className={'constitution-page'}>
+        <_PaperBorder/>
+        <h3 className={'centered page-title'}>Tutoring Calendar</h3>
+        <br/>
+          <Divider horizontal margin/>
+          <center>
+            <BigCalendar 
+              style={
+                {height: '60em',
+                width: '100%'}
+              }
+              events={this.state.events}/>
+            </center>
+    </div>
+    );
+  }
+}
+
+Tutoring = Guac(Tutoring);
+
+export default Tutoring;
+export {Tutoring};
---
+++ src/Pages/Tutoring/_PaperBorder/_PaperBorder.jsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+
+class _PaperBorder extends React.Component {
+  constructor() {
+    super();
+    this.render = this.render.bind(this);
+  }
+
+  render() {
+    return (
+      <div className={'paper-border'}>
+        <div className={'paper-canvas'}>
+          <div className={'main-paper z-depth-1'}>
+            <div className={'top-paper-1 z-depth-1'}/>
+            <div className={'top-paper-2 z-depth-1'}/>
+            <div className={'top-paper-3 z-depth-1'}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default _PaperBorder;
+export {_PaperBorder};
